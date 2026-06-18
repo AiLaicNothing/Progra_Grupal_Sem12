79f2b4c4f9af1901338fddbadb3343a9b328b1bf
Drocaud <megalapiz111@gmail.com>
2026-06-18 09:41:48 -0500

    StartNode Terminado
    Cree los 2 scripts están en la carpeta scripts, dentro de esa carpeta verás mi nombre.

---
+++ Assets/Scripts/Graphs/Editor/Nodes/Piero/StartNode.cs
@@ -0,0 +1,10 @@
+using System;
+
+[Serializable]
+public class StartNode : GenericNode
+{
+    protected override void OnDefinePorts(IPortDefinitionContext context)
+    {
+        context.AddOutputPort(OUTPUT_PORT_NAME).Build();
+    }
+}
