68dab9537a67799d00bbd33cc52a3721a0e39067
Drocaud <megalapiz111@gmail.com>
2026-06-24 15:00:46 -0500

    Actualización
    Ya cree el script GraphImporter actualmente esta incompleto debio a que falta algunos nodos.
    
    Cree los scripts DialogueRuntimeNode, NarrativeRuntimeNode y TransitionRuntimeNode.

---
+++ Assets/Scripts/Graphs/Editor/Runtime/Nodes/Piero/DialogueRuntimeNode.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class DialogueRuntimeNode : GenericRuntimeNode
+{
+    public string characterDialogue; 
+    public string textDialogue;
+}
---
+++ Assets/Scripts/Graphs/Editor/Runtime/Nodes/Piero/NarrativeRuntimeNode.cs
@@ -0,0 +1,7 @@
+using System;
+
+[Serializable]
+public class NarrativeRuntimeNode : GenericRuntimeNode
+{
+    public string textNarrative;
+}
